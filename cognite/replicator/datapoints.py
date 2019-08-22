b16c5a676af39eaa2ffbccf9cbb4e0b330803aeb
Torbjørn Opheim <torbjorn.opheim@cognite.com>
2019-08-22 15:16:48 +0200

    applied isort and black

--- cognite/replicator/datapoints.py
+++ cognite/replicator/datapoints.py
@@ -8,6 +8,7 @@ OPTIONAL: DATAPOINT_LIMIT (DEFAULT=10 000 000), keep_asset_connection (DEFAULT=T
 
 """
 import logging
+import multiprocessing as mp
 import time
 from datetime import datetime, timedelta
 from typing import Dict, List, Tuple, Union
@@ -15,12 +16,20 @@ from typing import Dict, List, Tuple, Union
 from cognite.client import CogniteClient
 from cognite.client.data_classes import TimeSeries
 from cognite.client.data_classes.assets import Asset
-import multiprocessing as mp
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
-def retrieve_insert(i,src_ext_id_list_parts,dst_ext_id_list,CLIENT_SRC, CLIENT_DST, keep_asset_connection, num_threads):
-    
+
+def retrieve_insert(
+    i,
+    src_ext_id_list_parts,
+    dst_ext_id_list,
+    CLIENT_SRC,
+    CLIENT_DST,
+    keep_asset_connection,
+    num_threads,
+):
+
     for src_ext_id in src_ext_id_list_parts[i]:
         if src_ext_id in dst_ext_id_list:
             # SOURCE
@@ -33,7 +42,9 @@ def retrieve_insert(i,src_ext_id_list_parts,dst_ext_id_list,CLIENT_SRC, CLIENT_D
                 )
                 continue
 
-            logging.debug(f"Latest timestamp source with ext_id {src_ext_id}: {latest_src_dp[0].timestamp}")
+            logging.debug(
+                f"Latest timestamp source with ext_id {src_ext_id}: {latest_src_dp[0].timestamp}"
+            )
             latest_src_time = latest_src_dp[0].timestamp
 
             # DESTINATION
@@ -52,11 +63,9 @@ def retrieve_insert(i,src_ext_id_list_parts,dst_ext_id_list,CLIENT_SRC, CLIENT_D
             logging.info(
                 f"Thread {i} is retrieving datapoints between {latest_dst_time} and {latest_src_time}\n-----------------------"
             )
-            
+
             datapoints = CLIENT_SRC.datapoints.retrieve(
-                external_id=src_ext_id,
-                start=latest_dst_time,
-                end=latest_src_time
+                external_id=src_ext_id, start=latest_dst_time, end=latest_src_time
             )
             logging.info(f"Number of datapoints: {len(datapoints)}")
             new_objects = [(o.timestamp, o.value) for o in datapoints]
@@ -65,11 +74,9 @@ def retrieve_insert(i,src_ext_id_list_parts,dst_ext_id_list,CLIENT_SRC, CLIENT_D
             latest_destination_dp = CLIENT_DST.datapoints.retrieve_latest(
                 external_id=src_ext_id
             )
-    
 
-def replicate(
-    CLIENT_SRC, CLIENT_DST, keep_asset_connection=True, num_threads=10
-):
+
+def replicate(CLIENT_SRC, CLIENT_DST, keep_asset_connection=True, num_threads=10):
 
     logging.info(f"Asset_connection is set to :{str(keep_asset_connection)}")
     ts_src = CLIENT_SRC.time_series.list(limit=None)
@@ -82,10 +89,23 @@ def replicate(
     dst_ext_id_list = [ts_id.external_id for ts_id in ts_dst]
     logging.debug(f"List of external id's in destination: {dst_ext_id_list}")
     step = len(ts_src) // num_threads
-    src_ext_id_list_parts = [src_ext_id_list[x:x+step] for x in range(0, len(src_ext_id_list), step)]
+    src_ext_id_list_parts = [
+        src_ext_id_list[x : x + step] for x in range(0, len(src_ext_id_list), step)
+    ]
     jobs = []
     for i in range(num_threads):
-        p = mp.Process(target=retrieve_insert, args=(i,src_ext_id_list_parts,dst_ext_id_list,CLIENT_SRC, CLIENT_DST, keep_asset_connection, num_threads))
+        p = mp.Process(
+            target=retrieve_insert,
+            args=(
+                i,
+                src_ext_id_list_parts,
+                dst_ext_id_list,
+                CLIENT_SRC,
+                CLIENT_DST,
+                keep_asset_connection,
+                num_threads,
+            ),
+        )
         jobs.append(p)
         p.start()
         p.join
