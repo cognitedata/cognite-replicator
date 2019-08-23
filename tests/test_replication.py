e5c67ea1a5abce87adcc0fe7f7942a72eaa9c455
Sofie Haug <sofie.haug@cognite.com>
2019-08-23 10:32:22 +0200

    Making more tests

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -2,6 +2,7 @@ from cognite.client.data_classes.assets import Asset
 from cognite.replicator.replication import make_id_object_map
 from cognite.replicator.replication import find_objects_to_delete_not_replicated_in_dst
 from cognite.replicator.replication import find_objects_to_delete_if_not_in_src
+from cognite.replicator.replication import existing_mapping
 
 
 def test_make_id_object_map():
@@ -9,6 +10,18 @@ def test_make_id_object_map():
     map = make_id_object_map(assets)
     assert 1 == len(map)
     assert 3 == map[55].id
+
+
+def test_existing_mapping():
+    assets = [
+        Asset(id=3, name="holy grenade", metadata={"_replicatedInternalId": 33}),
+        Asset(id=7, name="not holy grenade", parent_id=3, metadata={"_replicatedInternalId": 77}),
+        Asset(id=5, name="in-holy grenade", parent_id=7, metadata={"_replicatedInternalId": 55}),
+    ]
+    ids = existing_mapping(*assets)
+    assert ids[assets[0].metadata["_replicatedInternalId"]] == assets[0].id
+    assert ids[assets[1].metadata["_replicatedInternalId"]] == assets[1].id
+    assert ids[assets[2].metadata["_replicatedInternalId"]] == assets[2].id
 
 
 def test_find_objects_to_delete_not_replicated_in_dst():
