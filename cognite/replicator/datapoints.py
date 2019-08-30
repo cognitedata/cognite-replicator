ef5b5e464fe22e963c6213e3311b4ca49ca7f525
Torbjørn Opheim <torbjorn.opheim@cognite.com>
2019-08-30 15:55:53 +0200

    ran black

--- cognite/replicator/datapoints.py
+++ cognite/replicator/datapoints.py
@@ -25,7 +25,7 @@ def retrieve_insert(
     dst_ext_id_list: List[str],
     client_src: CogniteClient,
     client_dst: CogniteClient,
-    dp_limit: int
+    dp_limit: int,
 ):
 
     for src_ext_id in src_ext_id_list_parts:
@@ -33,17 +33,23 @@ def retrieve_insert(
             # SOURCE
             latest_src_dp = client_src.datapoints.retrieve_latest(external_id=src_ext_id)
             if not latest_src_dp:
-                logging.info(f"Thread {thread_id}: No datapoints found in source -- skipping time series associated with: {src_ext_id}")
+                logging.info(
+                    f"Thread {thread_id}: No datapoints found in source -- skipping time series associated with: {src_ext_id}"
+                )
                 continue
 
-            logging.debug(f"Thread {thread_id}: Latest timestamp source with ext_id {src_ext_id}: {latest_src_dp[0].timestamp}")
+            logging.debug(
+                f"Thread {thread_id}: Latest timestamp source with ext_id {src_ext_id}: {latest_src_dp[0].timestamp}"
+            )
             latest_src_time = latest_src_dp[0].timestamp
 
             # DESTINATION
             latest_destination_dp = client_dst.datapoints.retrieve_latest(external_id=src_ext_id)
             if not latest_destination_dp:
                 latest_dst_time = 0
-                logging.info(f"Thread {thread_id}: No datapoints in destination, starting copying from time(epoch): {latest_dst_time}")
+                logging.info(
+                    f"Thread {thread_id}: No datapoints in destination, starting copying from time(epoch): {latest_dst_time}"
+                )
             elif latest_destination_dp:
                 latest_dst_time = latest_destination_dp[0].timestamp
 
@@ -53,7 +59,7 @@ def retrieve_insert(
             )
 
             datapoints = client_src.datapoints.retrieve(
-                external_id=src_ext_id, start=latest_dst_time, end=latest_src_time, limit = dp_limit
+                external_id=src_ext_id, start=latest_dst_time, end=latest_src_time, limit=dp_limit
             )
             logging.info(f"Thread {thread_id}: Number of datapoints: {len(datapoints)}")
             new_objects = [(o.timestamp, o.value) for o in datapoints]
@@ -79,10 +85,7 @@ def replicate(client_src, client_dst, keep_asset_connection=True, num_threads=10
     jobs = []
     for thread_id in range(num_threads):
         parts = src_ext_id_list[thread_id * step : (thread_id + 1) * step]
-        p = mp.Process(
-            target=retrieve_insert,
-            args=(thread_id, parts, dst_ext_id_list, client_src, client_dst, limit),
-        )
+        p = mp.Process(target=retrieve_insert, args=(thread_id, parts, dst_ext_id_list, client_src, client_dst, limit))
         jobs.append(p)
         p.start()
     for p in jobs:
