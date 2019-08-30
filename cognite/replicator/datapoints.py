d4e3fa747a5b1cba6d3ea2b9c8cb58019280973a
Torbjørn Opheim <torbjorn.opheim@cognite.com>
2019-08-30 15:55:16 +0200

    changed default values to 10 threads and 100 000 data points

--- cognite/replicator/datapoints.py
+++ cognite/replicator/datapoints.py
@@ -4,7 +4,7 @@
 This script serves the purpose of replicating new datapoints from a source tenant to a destination tenant that holds corresponding time series.
 
 REQUIREMENTS: SAME external_id IN SRC AND DST TENANT, client_src, client_dst, SOURCE_API_KEY, DESTINATION_API_KEY
-OPTIONAL: keep_asset_connection (DEFAULT=True)
+OPTIONAL: keep_asset_connection (DEFAULT=True), change limit
 
 """
 import logging
@@ -25,8 +25,7 @@ def retrieve_insert(
     dst_ext_id_list: List[str],
     client_src: CogniteClient,
     client_dst: CogniteClient,
-    keep_asset_connection: bool,
-    num_threads: int,
+    dp_limit: int
 ):
 
     for src_ext_id in src_ext_id_list_parts:
@@ -34,17 +33,17 @@ def retrieve_insert(
             # SOURCE
             latest_src_dp = client_src.datapoints.retrieve_latest(external_id=src_ext_id)
             if not latest_src_dp:
-                logging.info(f"No datapoints found in source -- skipping time series associated with: {src_ext_id}")
+                logging.info(f"Thread {thread_id}: No datapoints found in source -- skipping time series associated with: {src_ext_id}")
                 continue
 
-            logging.debug(f"Latest timestamp source with ext_id {src_ext_id}: {latest_src_dp[0].timestamp}")
+            logging.debug(f"Thread {thread_id}: Latest timestamp source with ext_id {src_ext_id}: {latest_src_dp[0].timestamp}")
             latest_src_time = latest_src_dp[0].timestamp
 
             # DESTINATION
             latest_destination_dp = client_dst.datapoints.retrieve_latest(external_id=src_ext_id)
             if not latest_destination_dp:
                 latest_dst_time = 0
-                logging.info(f"No datapoints in destination, starting copying from time(epoch): {latest_dst_time}")
+                logging.info(f"Thread {thread_id}: No datapoints in destination, starting copying from time(epoch): {latest_dst_time}")
             elif latest_destination_dp:
                 latest_dst_time = latest_destination_dp[0].timestamp
 
@@ -54,16 +53,16 @@ def retrieve_insert(
             )
 
             datapoints = client_src.datapoints.retrieve(
-                external_id=src_ext_id, start=latest_dst_time, end=latest_src_time
+                external_id=src_ext_id, start=latest_dst_time, end=latest_src_time, limit = dp_limit
             )
-            logging.info(f"Number of datapoints: {len(datapoints)}")
+            logging.info(f"Thread {thread_id}: Number of datapoints: {len(datapoints)}")
             new_objects = [(o.timestamp, o.value) for o in datapoints]
             client_dst.datapoints.insert(new_objects, external_id=src_ext_id)
             # Update latest datapoint in destination for initializing next replication
             latest_destination_dp = client_dst.datapoints.retrieve_latest(external_id=src_ext_id)
 
 
-def replicate(client_src, client_dst, keep_asset_connection=True, num_threads=1):
+def replicate(client_src, client_dst, keep_asset_connection=True, num_threads=10, limit=100000):
 
     logging.info(f"Asset_connection is set to :{str(keep_asset_connection)}")
     ts_src = client_src.time_series.list(limit=None)
@@ -82,7 +81,7 @@ def replicate(client_src, client_dst, keep_asset_connection=True, num_threads=1)
         parts = src_ext_id_list[thread_id * step : (thread_id + 1) * step]
         p = mp.Process(
             target=retrieve_insert,
-            args=(thread_id, parts, dst_ext_id_list, client_src, client_dst, keep_asset_connection, num_threads),
+            args=(thread_id, parts, dst_ext_id_list, client_src, client_dst, limit),
         )
         jobs.append(p)
         p.start()
