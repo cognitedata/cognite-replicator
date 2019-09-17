4e29f93bb57ef5f030a87ad61a76ede50b92904f
Tristan Monger <tristan.monger@cognite.com>
2019-09-17 18:01:29 +0200

    Update timeseries filter test

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -1,5 +1,5 @@
 from cognite.client.data_classes import TimeSeries
-from cognite.replicator.time_series import filter_away_service_account_ts
+from cognite.replicator.time_series import filter_non_copyable_ts
 
 
 def test_filter_away_service_account_ts():
@@ -7,7 +7,12 @@ def test_filter_away_service_account_ts():
         TimeSeries(name="holy_timeseries_service_account_metrics", metadata={}),
         TimeSeries(name="not holy timeseries service_account_metrics", metadata={}),
         TimeSeries(name="in-holy timeseries", metadata={}),
+        TimeSeries(name="secure timeseries", metadata={}, security_categories=[2]),
+        TimeSeries(name="insecure timeseries 1", metadata={}, security_categories=[]),
+        TimeSeries(name="insecure timeseries 2", metadata={}),
     ]
-    ts_list = filter_away_service_account_ts(ts_src)
-    assert len(ts_list) == 1
+    ts_list = filter_non_copyable_ts(ts_src)
+    assert len(ts_list) == 3
     assert ts_list[0].name == "in-holy timeseries"
+    assert ts_list[1].name == "insecure timeseries 1"
+    assert ts_list[2].name == "insecure timeseries 2"
