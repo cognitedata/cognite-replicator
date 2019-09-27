e206c1b1f1886d5d7d60f4f9c9d72d1c5e582454
Tristan Monger <54885797+tristan-cognite@users.noreply.github.com>
2019-09-27 13:24:29 +0200

    Add batch sizing arg; improve logging (#82)
    * Improve logging
    * Switch option num_batch to batch_size
    * Bump patch version

--- cognite/replicator/datapoints.py
+++ cognite/replicator/datapoints.py
@@ -2,6 +2,8 @@
 
 import logging
 import multiprocessing as mp
+from datetime import datetime
+from math import ceil, floor
 from typing import Any, List, Optional
 
 from cognite.client import CogniteClient
@@ -31,62 +33,101 @@ def retrieve_insert(
         mock_run: If true, only retrieves data points from source and does not insert into destination
     """
 
-    logging.info(f"Job {job_id}: Starting datapoint replication...")
+    def handle_failed_timeseries(external_id: str, e: Exception, failed_lst: List[str]):
+        failed_lst.append(external_id)
+        logging.error(f"Job {job_id}: Failed for external id {external_id}. {e}")
+
+    logging.info(f"Job {job_id}: Starting datapoint replication for {len(ext_ids)} time series...")
     updated_timeseries_count = 0
     total_datapoints_copied = 0
+    empty_external_ids = []
     failed_external_ids = []
-    for ext_id in ext_ids:
+    start_time = datetime.now()
+    for i, ext_id in enumerate(ext_ids):
+        if ext_id is None:
+            continue
+
+        if i % ceil(len(ext_ids) / 10) == 0:
+            logging.info(
+                f"Job {job_id}: Progress: On time series {i+1}/{len(ext_ids)} "
+                f"({floor(100 * i / len(ext_ids))}% complete)"
+                f" in {datetime.now()-start_time}"
+            )
+            logging.info(
+                f"Job {job_id}: Current results: {updated_timeseries_count} time series updated, "
+                f"{i - updated_timeseries_count - len(failed_external_ids) - len(empty_external_ids)} time "
+                f"series up-to-date. "
+                f"{total_datapoints_copied} datapoints copied. "
+                f"{len(failed_external_ids)} failure(s). {len(empty_external_ids)} time series without datapoints."
+            )
+
+        # SOURCE
         try:
-            # SOURCE
             latest_src_dp = client_src.datapoints.retrieve_latest(external_id=ext_id)
-            if not latest_src_dp:
-                logging.info(
-                    f"Job {job_id}: No datapoints found in source -- " f"skipping time series associated with: {ext_id}"
-                )
-                continue
+        except CogniteAPIError as exc:
+            handle_failed_timeseries(ext_id, exc, failed_external_ids)
+            continue
 
-            logging.debug(f"Job {job_id}: Latest timestamp source with ext_id {ext_id}: {latest_src_dp[0].timestamp}")
-            latest_src_time = latest_src_dp[0].timestamp + 1  # +1 because end time is exclusive for retrieve()
+        if not latest_src_dp:
+            logging.debug(
+                f"Job {job_id}: No datapoints found in source -- skipping time series associated with: {ext_id}"
+            )
+            empty_external_ids.append(ext_id)
+            continue
 
-            # DESTINATION
+        logging.debug(f"Job {job_id}: Latest timestamp source with ext_id {ext_id}: {latest_src_dp[0].timestamp}")
+        latest_src_time = latest_src_dp[0].timestamp + 1  # +1 because end time is exclusive for retrieve()
+
+        # DESTINATION
+        try:
             latest_destination_dp = client_dst.datapoints.retrieve_latest(external_id=ext_id)
-            latest_dst_time = 0
-            if not latest_destination_dp:
-                logging.debug(
-                    f"Job {job_id}: No datapoints in destination, "
-                    f"starting copying from time(epoch): {latest_dst_time}"
-                )
-            elif latest_destination_dp:
-                # +1 because start time is inclusive for retrieve()
-                latest_dst_time = latest_destination_dp[0].timestamp + 1
+        except CogniteAPIError as exc:
+            handle_failed_timeseries(ext_id, exc, failed_external_ids)
+            continue
 
-            if latest_dst_time >= latest_src_time:
-                logging.debug(f"Skipping {ext_id} because already up-to-date")
-                continue
+        latest_dst_time = 0
+        if not latest_destination_dp:
+            logging.debug(
+                f"Job {job_id}: No datapoints in destination, starting copying from time(epoch): {latest_dst_time}"
+            )
+        elif latest_destination_dp:
+            # +1 because start time is inclusive for retrieve()
+            latest_dst_time = latest_destination_dp[0].timestamp + 1
 
-            # Retrieve and insert missing datapoints
-            logging.debug(f"Job {job_id} is retrieving datapoints between {latest_dst_time} and {latest_src_time}")
+        if latest_dst_time >= latest_src_time:
+            logging.debug(f"Job {job_id}: Skipping {ext_id} because already up-to-date")
+            continue
 
+        # Retrieve and insert missing datapoints
+        logging.debug(f"Job {job_id}: Retrieving datapoints between {latest_dst_time} and {latest_src_time}")
+
+        try:
             datapoints = client_src.datapoints.retrieve(
                 external_id=ext_id, start=latest_dst_time, end=latest_src_time, limit=limit
             )
             logging.debug(f"Job {job_id}: Number of datapoints: {len(datapoints)}")
             if not mock_run:
                 client_dst.datapoints.insert(datapoints, external_id=ext_id)
-            updated_timeseries_count += 1
-            total_datapoints_copied += len(datapoints)
-        except CogniteAPIError:
-            failed_external_ids.append(ext_id)
+        except CogniteAPIError as exc:
+            handle_failed_timeseries(ext_id, exc, failed_external_ids)
+            continue
+
+        updated_timeseries_count += 1
+        total_datapoints_copied += len(datapoints)
 
     logging.info(
-        f"Job {job_id}: Done! {updated_timeseries_count} time series updated, "
-        f"{len(ext_ids) - updated_timeseries_count} time series up-to-date. "
+        f"Job {job_id}: Done! Final results: {updated_timeseries_count} time series updated, "
+        f"{len(ext_ids) - updated_timeseries_count - len(failed_external_ids) - len(empty_external_ids)} time series "
+        f"up-to-date. "
         f"{total_datapoints_copied} datapoints copied in total. "
-        f"{len(failed_external_ids)} failure(s)."
+        f"{len(failed_external_ids)} failure(s). {len(empty_external_ids)} time series without datapoints."
     )
 
     if len(failed_external_ids):
-        logging.error(f"Sample of failed ids: {failed_external_ids[:min(10, len(failed_external_ids))]}")
+        logging.error(f"Job {job_id}: Sample of failed ids: {failed_external_ids[:10]}")
+
+    if len(empty_external_ids):
+        logging.info(f"Job {job_id}: Sample of empty time series: {empty_external_ids[:10]}")
 
 
 def _get_chunk(lst: List[Any], num_chunks: int, chunk_number: int) -> List[Any]:
@@ -114,8 +155,8 @@ def _get_chunk(lst: List[Any], num_chunks: int, chunk_number: int) -> List[Any]:
 def replicate(
     client_src: CogniteClient,
     client_dst: CogniteClient,
+    batch_size: Optional[int] = None,
     num_threads: int = 10,
-    num_batches: Optional[int] = None,
     limit: Optional[int] = None,
     external_ids: Optional[List[str]] = None,
     mock_run: bool = False,
@@ -127,8 +168,8 @@ def replicate(
     Args:
         client_src: The client corresponding to the source project.
         client_dst: The client corresponding to the destination project.
+        batch_size: The size of batches to split the external id list into. Defaults to num_threads.
         num_threads: The number of threads to be used.
-        num_batches: The amount of batches to split the external id list into. Defaults to num_threads.
         limit: The maximum number of data points to copy per time series
         external_ids: A list of time series to replicate data points for
         mock_run: If true, runs the replication without insert, printing what would happen
@@ -149,9 +190,9 @@ def replicate(
     logging.info(
         f"Number of common time series external ids between destination and source: {len(shared_external_ids)}"
     )
-    if num_batches is None:
-        num_batches = num_threads
-    num_batches = min(num_batches, len(shared_external_ids))
+    if batch_size is None:
+        batch_size = max(1, len(shared_external_ids) // num_threads)
+    num_batches = ceil(len(shared_external_ids) / batch_size)
 
     arg_list = [
         (client_src, client_dst, job_id, _get_chunk(shared_external_ids, num_batches, job_id), limit, mock_run)
