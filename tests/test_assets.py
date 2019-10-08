5e829bb8177e104368641b0bfa7a5636b3b8d3fe
Tristan Monger <tristan.monger@cognite.com>
2019-10-08 15:41:07 +0200

    Add preliminary sequence tests

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,7 +1,8 @@
 import time
 
+from cognite.client import CogniteClient
 from cognite.client.data_classes.assets import Asset
-from cognite.client.testing import mock_cognite_client
+from cognite.client.testing import monkeypatch_cognite_client
 from cognite.replicator.assets import build_asset_create, build_asset_update, create_hierarchy, find_children
 
 
@@ -71,7 +72,8 @@ def test_build_asset_update():
 
 
 def test_create_hierarchy_without_dst_list():
-    with mock_cognite_client() as client:
+    with monkeypatch_cognite_client():
+        c = CogniteClient()
         runtime = time.time() * 1000
         assets_src = [
             Asset(
