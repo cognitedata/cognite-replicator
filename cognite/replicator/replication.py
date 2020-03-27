1b1866842055367780c0b434d728e7dc47b1e272
Gaetan Helness <gaetan.helness@cognite.com>
2020-03-27 13:03:51 +0100

    Update replication.py

--- cognite/replicator/replication.py
+++ cognite/replicator/replication.py
@@ -203,7 +203,10 @@ def make_objects_batch(
                 unchanged_objects.append(dst_obj)
         elif dst_ts:
             if src_obj.external_id in dst_ts_ext_id_set:
-                unchanged_objects.append(dst_obj)
+                unchanged_objects.append(src_obj)
+            else:
+                new_asset = create(src_obj, src_dst_ids_assets, project_src, replicated_runtime, **kwargs)
+                create_objects.append(new_asset)
         else:
             new_asset = create(src_obj, src_dst_ids_assets, project_src, replicated_runtime, **kwargs)
             create_objects.append(new_asset)
