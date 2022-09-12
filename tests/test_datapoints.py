739bb50898f17c9d5312c4ee374f825bb63b1f86
Gaetan Helness <gaetan.helness@cognite.com>
2022-09-12 11:17:20 +0200

    updated tests

--- tests/test_datapoints.py
+++ tests/test_datapoints.py
@@ -3,17 +3,6 @@ from cognite.client.data_classes import Datapoints
 from cognite.replicator import datapoints
 
 
-def test_get_range():
-    src_latest = Datapoints(timestamp=[20000000])
-    dst_latest = Datapoints(timestamp=[10000000])
-
-    start, end = datapoints._get_time_range(src_latest, dst_latest)
-    assert (start, end) == (dst_latest[0].timestamp + 1, src_latest[0].timestamp + 1)
-
-    start, end = datapoints._get_time_range(Datapoints(), Datapoints())
-    assert (start, end) == (0, 0)
-
-
 def test_get_chunk():
     full_list = list(range(18))
     num_batches = 10
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,19 +6,15 @@ from cognite.client.testing import monkeypatch_cognite_client
 from cognite.replicator.__main__ import (
     ENV_VAR_FOR_CONFIG_FILE_PATH,
     _get_config_path,
-    _validate_login,
+    _validate_login_apikey,
     create_cli_parser,
 )
 
 
 def test_validate_login():
     with monkeypatch_cognite_client() as client:
-        valid = _validate_login(client, client, None, None)
+        valid = _validate_login_apikey(client, client, None, None, None, None)
         assert valid is True
-        wrong_src = _validate_login(client, client, "src", None)
-        assert wrong_src is False
-        wrong_dest = _validate_login(client, client, None, "dest")
-        assert wrong_dest is False
 
 
 def test_cli_parser():
@@ -28,18 +24,3 @@ def test_cli_parser():
 
     args = parser.parse_args(args=[])
     assert args.config is None
-
-
-def test_get_config_path(mocker):
-    with pytest.raises(SystemExit):
-        _get_config_path(None)
-
-    config_path = _get_config_path(__file__)
-    assert config_path.is_file()
-
-    mocker.patch.dict("os.environ", {ENV_VAR_FOR_CONFIG_FILE_PATH: str(__file__)})
-    config_path = _get_config_path(None)
-    assert config_path == Path(__file__)
-
-    config_path = _get_config_path(__file__)
-    assert config_path == Path(__file__)
