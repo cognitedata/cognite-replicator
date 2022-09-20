26c4f5a684384597e32edf6625563429aad5e448
Pierre Pernot <pierre.pernot@cognite.com>
2022-09-20 10:34:54 +0200

    optimization of timeseries replication

--- cognite/replicator/time_series.py
+++ cognite/replicator/time_series.py
@@ -248,7 +248,8 @@ def replicate(
     src_dst_ids_assets = replication.existing_mapping(*assets_dst)
 
     if not src_dst_ids_assets:
-        assets_src = client_src.assets.list(limit=None)
+        assets_src_ids = list(set(map(lambda x: x.asset_id, ts_src)))
+        assets_src = client_src.assets.retrieve_multiple(ids=assets_src_ids, ignore_unknown_ids=True)
         src_assets_map = replication.make_external_id_obj_map(assets_src)
         src_dst_ids_assets = replication.map_ids_from_external_ids(src_assets_map, assets_dst)
 
